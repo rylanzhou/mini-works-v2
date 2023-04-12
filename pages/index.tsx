e6e1247d8a863815cf9b8802967f2e3895302e98
Rylan Zhou <rylanzhou@outlook.com>
2023-04-12 16:00:50 -0700

    chore: add page title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
+import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useCallback, useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react';
 import {
   FaAddressBook,
   FaBalanceScale,
